771fe231b512d148fc523143b7e8f5017cde277f
Rafael Correr <119776059+WindFox8@users.noreply.github.com>
2024-03-03 17:21:14 -0300

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,53 @@
+const phrases = [
+    "FRONT-END DEVELOPER",
+    "WEB DESIGNER",
+    "SOFTWARE ENGINEER"
+  ];
+
+  let index = 0;
+  let isDeleting = false;
+
+  function animateText() {
+    const phraseElement = document.getElementById("phrase");
+    const cursorElement = document.getElementById("cursor");
+    let currentPhrase = phrases[index];
+
+    if (!currentPhrase) {
+      index = 0;
+      currentPhrase = phrases[index];
+    }
+
+    let i = isDeleting ? currentPhrase.length : 0;
+    const delta = isDeleting ? -1 : 1;
+
+    const interval = setInterval(() => {
+      phraseElement.textContent = currentPhrase.substring(0, i);
+
+      if (isDeleting) {
+        cursorElement.style.visibility = "hidden";
+      } else {
+        cursorElement.style.visibility = "visible";
+      }
+
+      if (isDeleting && i === 0) {
+        clearInterval(interval);
+        isDeleting = false;
+        setTimeout(() => {
+          index++;
+          animateText();
+        }, 500);
+      } else if (!isDeleting && i === currentPhrase.length) {
+        clearInterval(interval);
+        isDeleting = true;
+        setTimeout(() => {
+          animateText();
+        }, 1000);
+      }
+
+      i += delta;
+    }, 150);
+  }
+
+  animateText();
+
+  
